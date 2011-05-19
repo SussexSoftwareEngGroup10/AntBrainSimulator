b3340f6fbf4071e06a62f01ccc63b25200c41459
Michael Stocks <michaelstocks1@gmail.com>
2011-05-19 15:12:42 +0000

    Updated world tests

--- src/antWorld/WorldTest.java
+++ src/antWorld/WorldTest.java
@@ -27,7 +27,7 @@ public class WorldTest {
 		
 			assertTrue("ADD MORE TESTS",testWorld != null);
 		} catch (IOEvent e) {
-			// TODO Auto-generated catch block
+			
 			e.printStackTrace();
 		}
 	}
@@ -42,32 +42,42 @@ public class WorldTest {
 			assertEquals("ADD MORE TESTS",10, worldSize.length);
 			//the example world is size 10, that is what should be returned
 		} catch (IOEvent e) {
-			// TODO Auto-generated catch block
+			
 			e.printStackTrace();
 		}
 	}
 	
 	@Test
-	public void testReturnAnts(){
+	public void testReturnAntsInvalidWorld(){
 		try {
 			testWorld = WorldParser.readWorldFromCustom("example");
 			Ant[] testAnts = testWorld.getAnts();
-			assertEquals(32, testAnts.length);
-			//TODO: Works out length of hexagon with length 5
+			assertEquals("FAILURE: FAILED BECAUSE INVALID WORLD", 32, testAnts.length);
+			//Works out length of hexagon with length
 		} catch (IOEvent e) {
-			// TODO Auto-generated catch block
+			
 			e.printStackTrace();
 		}
+	}
+	
+	@Test
+	public void testReturnAntsValidWorld(){
+		try {
+				testWorld = World.getContestWorld(93745);
+				Ant[] testAnts = testWorld.getAnts();
+				assertEquals(254, testAnts.length);
+				} catch (ErrorEvent e) {
+		
+					e.printStackTrace();
+				}
 	}
 	
 	@Test
 	public void testReturnFood(){
 		try {
 			testWorld = WorldParser.readWorldFromCustom("example");
-		
-		fail("FINISH ME");
+			fail("FINISH ME");
 		} catch (IOEvent e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
@@ -79,7 +89,7 @@ public class WorldTest {
 		//TODO:TAKE OUT PRINnt
 		System.out.println(testWorld);
 		} catch (ErrorEvent e) {
-			// TODO Auto-generated catch block
+			
 			e.printStackTrace();
 		}
 	}

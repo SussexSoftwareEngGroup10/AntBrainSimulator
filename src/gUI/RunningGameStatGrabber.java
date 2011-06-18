326002f5128cc0e8bb79fc7e84e757d2b1657019
Will Sewell <me@willsewell.name>
2011-06-18 13:16:37 +0000

    testing...

---
+++ src/gUI/RunningGameStatGrabber.java
@@ -0,0 +1,19 @@
+package gUI;
+
+import engine.GameEngine;
+
+public class RunningGameStatGrabber implements Runnable {
+	MainWindow mainWindow;
+	GameEngine gameEngine;
+	
+	public RunningGameStatGrabber(MainWindow mainWindow, 
+								  GameEngine gameEngine) {
+		this.mainWindow = mainWindow;
+		this.mainWindow = mainWindow;
+	}
+	
+	public void run() {
+		// TODO Auto-generated method stub
+	}
+
+}
